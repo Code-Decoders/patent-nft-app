678db3e0c919dedb1c7d665341eef441c4e24d91
kunal528 <64953335+kunal528@users.noreply.github.com>
2022-11-11 20:13:44 +0530

    card

--- components/card.js
+++ components/card.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { global } from 'styled-jsx/css'
 import styles from '../styles/Card.module.css'
 
 const Card = () => {
--- pages/nfts/[id].js
+++ pages/nfts/[id].js
@@ -1,8 +1,23 @@
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useEffect, useRef } from 'react'
+import styles from '../../styles/NFTPage.module.css'
 
 const NFTPage = () => {
+    const router = useRouter()
+    const { id } = router.query
+    
     return (
-        <div>NFTPage</div>
+        <div className={styles.container}>
+            <video className={styles.video} src="http://localhost:3000/videos/demo.mp4" autoplay="true"
+                muted="muted" />
+            <div>
+                <div className={styles.title}>Rappu Apes</div>
+                <div className={styles.creator}>Created by Daq</div>
+                <div className={styles.offer}>Latest Offer</div>
+                <div className={styles.offer}>3.48 ETH</div>
+                
+            </div>
+        </div>
     )
 }
 
